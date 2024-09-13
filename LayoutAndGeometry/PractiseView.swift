e99bea8ab9a338df51e40eab4af34f997cb11473
QinYYY <qinyang_c@outlook.com>
2024-09-13 17:15:53 +0930

    Layout and Geometry

---
+++ LayoutAndGeometry/PractiseView.swift
@@ -0,0 +1,18 @@
+//
+//  PractiseView.swift
+//  LayoutAndGeometry
+//
+//  Created by QinY on 13/9/2024.
+//
+
+import SwiftUI
+
+struct PractiseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PractiseView()
+}
